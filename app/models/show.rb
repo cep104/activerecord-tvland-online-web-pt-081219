7937a204b9c783f3d075f55343cc9928c4c78280
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-18 23:01:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   def actors_list
-    self.actors.full_name
+    self.actors do |t|
+      t.full_name 
+    end
   end
 end
