22cbbf4148a1c88e00c5b993af6c54763ac0cd4c
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-18 22:46:09 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
@@ -8,5 +9,6 @@ class Actor < ActiveRecord::Base
   
   def list_roles 
     self.characters.name
+    binding.pry
   end
 end
